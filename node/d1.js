d6e1535980b55ab76aa98dfcbcf58c888e80103e
tbielaszewski <tomekbielaszewski@gmail.com>
2019-12-01 14:39:42 +0100

    d1 second solution

--- node/d1.js
+++ node/d1.js
@@ -4,12 +4,18 @@ function calcFuel(mass) {
   return Math.floor(mass/3) -2;
 }
 
+function calcExtraFuel(mass) {
+  let fuel = calcFuel(mass);
+  if(fuel > 0) return fuel + calcExtraFuel(fuel);
+  else return 0;
+}
+
 function solution1(input) {
   return _.sum(input.map(calcFuel));
 }
 
 function solution2(input) {
-
+  return _.sum(input.map(calcExtraFuel));
 }
 
 module.exports = {
