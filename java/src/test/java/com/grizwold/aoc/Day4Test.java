41d8b343a83d884af578e7d9b84494986ddb1459
tbielaszewski <tomekbielaszewski@gmail.com>
2019-12-08 12:49:48 +0100

    java day 4 solution

--- java/src/test/java/com/grizwold/aoc/Day4Test.java
+++ java/src/test/java/com/grizwold/aoc/Day4Test.java
@@ -3,6 +3,8 @@ package com.grizwold.aoc;
 import org.hamcrest.core.Is;
 import org.junit.Test;
 
+import java.util.stream.IntStream;
+
 import static org.junit.Assert.assertThat;
 
 public class Day4Test {
@@ -13,6 +15,15 @@ public class Day4Test {
         example("111111", true);
         example("223450", false);
         example("123789", false);
+    }
+
+    @Test
+    public void solution() {
+        System.out.println("Solution for day 4 part 1 is: " +
+                IntStream.range(357253, 892943)
+                        .filter(i -> day4.isValid(String.valueOf(i)))
+                        .count()
+        );
     }
 
     private void example(String pass, boolean valid) {
